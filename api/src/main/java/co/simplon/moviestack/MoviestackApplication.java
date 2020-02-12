1618f7d302c87f8d91b8349a443f70059c95c400
SylvainSimplonDev <sylvain.grippon.devweb@gmail.com>
2020-02-12 15:30:52 +0100

    Add API first commit

---
+++ api/src/main/java/co/simplon/moviestack/MoviestackApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.moviestack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviestackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviestackApplication.class, args);
+    }
+
+}
