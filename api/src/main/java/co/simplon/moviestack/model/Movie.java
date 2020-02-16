febbdacd214f61cee5650185409b3b9d7d2def95
CiGit-Franck <franck.thery@gmail.com>
2020-02-16 19:44:47 +0100

    [US-02-001] init branch

---
+++ api/src/main/java/co/simplon/moviestack/model/Movie.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.simplon.moviestack.model;
+
+/**
+ *
+ * @author Franck
+ */
+public class Movie {
+    
+}
