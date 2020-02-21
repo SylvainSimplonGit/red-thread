57e21726f9c6a95db9e23cbd8c7b4820192e35a5
Nicolas RAOUL <nicolas.raoul@renault.com>
2020-02-21 16:15:44 +0100

    Add Genre enum

---
+++ api/src/main/java/co/simplon/moviestack/model/Genre.java
@@ -0,0 +1,39 @@
+package co.simplon.moviestack.model;
+
+public enum Genre {
+    ACTION("Action"),
+    AVENTURE("Adventure"),
+    ANIME("Animation"),
+    COMEDIE("Comedy"),
+    CRIME("Crime"),
+    DOCU("Documentary"),
+    DRAME("Drama"),
+    FAMILLE("Family"),
+    FANTASTIQUE("Fantasy"),
+    HISTOIRE("History"),
+    HORREUR("Horror"),
+    MUSIQUE("Music"),
+    MYSTERE("Mystery"),
+    ROMANCE("Romance"),
+    SCIENCE_FICTION("Sci-Fi"),
+    THRILLER("Thriller"),
+    WAR("War"),
+    WESTERN("Western");
+
+
+    private String genre;
+
+    Genre (String genre) {this.genre = genre;}
+
+    public String toString(){
+        return genre;
+    }
+
+
+
+
+
+
+
+
+}
