0edc18fabc5c3d3e2a9e3e280f754254d22535ba
Nicolas RAOUL <nicolas.raoul@renault.com>
2020-03-05 15:27:00 +0100

    Finition of TopBar and Footer with colors and framing

--- ihm/src/app/app.module.ts
+++ ihm/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopBarComponent
+    TopBarComponent,
+    WelcomePageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([{path: '', component: WelcomePageComponent},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
