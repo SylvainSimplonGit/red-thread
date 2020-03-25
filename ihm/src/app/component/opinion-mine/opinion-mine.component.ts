b62e517d4287a498090ced6022f04b7e8fa0a7ad
SylvainSimplonDev <sylvain.grippon.devweb@gmail.com>
2020-03-25 15:20:44 +0100

    Bug sur retour d'info sur movie-sheet

--- ihm/src/app/component/opinion-mine/opinion-mine.component.ts
+++ ihm/src/app/component/opinion-mine/opinion-mine.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import {Component, Inject, OnInit, Output} from '@angular/core';
 // import { MatTableDataSource } from '@angular/material/table';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { FormBuilder, FormGroup } from '@angular/forms';
@@ -10,28 +10,13 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class OpinionMineComponent implements OnInit {
 
-  // form: FormGroup;
-  // title: string;
-  // myOpinion: string;
-  // opinionsList: [];
-  // displayedColumns: string[] = ['movieBuff', 'rating', 'comment'];
-  // dataSource = new MatTableDataSource(this.opinionsMine);
-
-  // @ViewChild(MatSort, {static: false}) sort: MatSort;
-
   myOpinionForm: FormGroup;
-  //   opinion: data.opinion
-  // });
 
   constructor(
     private dialogRef: MatDialogRef<OpinionMineComponent>,
     private formBuilder: FormBuilder,
     @Inject(MAT_DIALOG_DATA) public data: DialogData
   ) {
-    // this.title = data.title;
-    // this.myOpinion = data.opinion;
-    // this.opinionsMine = data.opinionMine;
-
     this.myOpinionForm = this.formBuilder.group({
       opinion: data.opinion
     });
@@ -39,12 +24,7 @@ export class OpinionMineComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    // this.dataSource.sort = this.sort;
   }
-
-  // save() {
-  //   this.dialogRef.close();
-  // }
 
   close() {
     this.dialogRef.close();
